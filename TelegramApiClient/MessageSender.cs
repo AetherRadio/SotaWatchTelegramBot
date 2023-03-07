1716fbb59e16255c22a1e9d3f1ee2f97a6aeaf48
Rui Oliveira <ruimail24@gmail.com>
2023-03-07 09:34:49 +0000

    Create the MessageSender for Telegram

---
+++ TelegramApiClient/MessageSender.cs
@@ -0,0 +1,30 @@
+﻿// This file is part of Aether Radio's SOTA Watch Telegram Bot.
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2023 Rui Oliveira <ruimail24@gmail.com>
+
+namespace AetherRadio.SotaWatchTelegramBot.TelegramApiClient;
+
+public class MessageSender
+{
+    private readonly HttpClient httpClient;
+    private IEnumerable<string> ChatIdsToSendTo { get; set; }
+
+    public MessageSender(string botToken, IEnumerable<string> chatIdsToSendTo)
+    {
+        httpClient = new() { BaseAddress = new Uri($"https://api.telegram.org/bot{botToken}/") };
+        ChatIdsToSendTo = chatIdsToSendTo;
+    }
+
+    public void Send(string message)
+    {
+
+        foreach (string channel in ChatIdsToSendTo)
+        {
+            string requestUrlSuffix = $"sendMessage?chat_id={channel}&text={message}";
+
+            httpClient.GetAsync(requestUrlSuffix);
+
+            // TODO: check response for errors
+        }
+    }
+}
